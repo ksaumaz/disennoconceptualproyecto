d352521fd59e51c4120ad6e94dd91a8b38266832
David <drodriguezc1@ucenfotec.ac.cr>
2022-04-22 12:25:21 -0600

    Parte del proyecto

--- proyecto/src/main/java/ac/cr/ucenfotec/calculoprestamo/SumaPrestamo.java
+++ proyecto/src/main/java/ac/cr/ucenfotec/calculoprestamo/SumaPrestamo.java
@@ -10,12 +10,6 @@ import javax.inject.Named;
 public class MontoTotal implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        String montoMateriasCronograma = (String) execution.getVariable("montoMateriasCronograma");
-        String montoMatriculaCronograma = (String) execution.getVariable("montoMatriculaCronograma");
-        String montoSostenimientoCronograma = (String) execution.getVariable("montoSostenimientoCronograma");
-        int montoFinanciamientoMatricula = Integer.parseInt(montoMateriasCronograma) + Integer.parseInt(montoMatriculaCronograma) + Integer.parseInt(montoSostenimientoCronograma);
-        execution.setVariable("montoFinanciamientoMatricula", montoFinanciamientoMatricula);
-        System.out.println("Cálculo de cuota fiduciaria total: " + montoFinanciamientoMatricula);
         String montoFinanciamientoMatricual = (String) execution.getVariable("montoFinanciamientoMatricula");
         String montoOtrosFinanciamientos = (String) execution.getVariable("montoOtrosFinanciamientos");
         int montoFinanciamientoTotal = Integer.parseInt(montoFinanciamientoMatricual) + Integer.parseInt(montoOtrosFinanciamientos);
